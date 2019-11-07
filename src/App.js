1c53bef5a4cf06b2f4d5dcea3b87ab875ac9653f
Psage23 <pamelasage3@gmail.com>
2019-11-06 17:12:15 -0700

    completed phase one

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import PhotoGrid from "./components/PhotoGrid";
 import "./App.css";
-import { tsPropertySignature } from "@babel/types";
+
+
 
 function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=VvxYi49718Og9NoNdtvI3xrBx1bgHYhulAsLR3JB`)
-    .then(response => {
-        console.log(response)
-    })
-    .catch(error => {
-        console.log("No data found", error)
-    })
-},[])
 
   return (
     <div className="App">
--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -1,11 +1,28 @@
-import React from "react";
-import PhotoCard from "./PhotoCard";
+import React, {useState, useEffect} from "react";
+import axios from "axios"
+
 
 
 export default function PhotoGrid() {
-   
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=VvxYi49718Og9NoNdtvI3xrBx1bgHYhulAsLR3JB`)
+        .then(response => {
+            console.log(response.data)
+            setData(response.data)
+        })
+        .catch(error => {
+            console.log("No data found", error)
+        })
+    },[])
 
     return (
-        <div>PhotoCard</div>
+        <div className="container">
+            <img className="picture" alt="Space view of the day" src={data.url}></img>
+            <h2>Title: {data.title}</h2>
+            <p>Today's Date: {data.date}</p>
+            <p>Description: {data.explanation}</p>
+        </div>
     )
 }
