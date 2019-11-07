5cc5f8b4333949433f5c81f88577c4d90064d48b
Psage23 <pamelasage3@gmail.com>
2019-11-07 13:50:50 -0700

    used strap for styling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import PhotoGrid from "./components/PhotoGrid";
 import "./App.css";
 
-
-
 function App() {
 
   return (
--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -1,9 +1,8 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
-import styled from 'styled-components';
 import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button
+  Card, CardText, CardBody,
+  CardTitle, CardSubtitle, CardLink,
 } from 'reactstrap';
 
 
@@ -29,14 +28,28 @@ export default function PhotoGrid() {
         //     <p>Today's Date: {data.date}</p>
         //     <p>Description: {data.explanation}</p>
         // </div>
-        <Card>
-        <CardImg top width="100%" src={data.url} alt="Space view of the day" />
-        <CardBody>
-          <CardTitle>Title: {data.title}</CardTitle>
-          <CardSubtitle>Today's Date: {data.date}</CardSubtitle>
-          <CardText>Description: {data.explanation}</CardText>
-          <Button>Button</Button>
-        </CardBody>
+      //   <Card>
+      //   <CardImg top width="100%" src={data.url} alt="Space view of the day" />
+      //   <CardBody>
+      //     <CardTitle>Title: {data.title}</CardTitle>
+      //     <CardSubtitle>Today's Date: {data.date}</CardSubtitle>
+      //     <CardText>Description: {data.explanation}</CardText>
+      //     <Button>Button</Button>
+      //   </CardBody>
+      // </Card>
+      <div className="container">
+      <Card>
+      <CardBody>
+        <CardTitle><h2>Title:</h2>{data.title}</CardTitle>
+        <CardSubtitle><h2>Today's Date:</h2>{data.date}</CardSubtitle>
+      </CardBody>
+      <img width="100%" src={data.url} alt="Space view of the day" />
+      <CardBody>
+        <CardText><h2>Description:</h2>{data.explanation}</CardText>
+        <CardLink href="https://apod.nasa.gov/apod/calendar/allyears.html">Calendar of Nasa Photos of the Day</CardLink>
+        <CardLink href="https://apod.nasa.gov/apod/lib/edlinks.html">Learn More about Space</CardLink>
+      </CardBody>
       </Card>
+      </div>
     )
 }
